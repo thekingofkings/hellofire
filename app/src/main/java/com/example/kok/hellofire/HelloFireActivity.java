4bc6a99dc79f4d7c610449760419abfde7d6ee29
Hongjian <hongjian-wang@outlook.com>
2018-11-10 12:28:48 -0800

    Upgrade to Android API 23; Add camera permission; Add flashlight control

--- app/src/main/java/com/example/kok/hellofire/HelloFireActivity.java
+++ app/src/main/java/com/example/kok/hellofire/HelloFireActivity.java
@@ -1,29 +1,56 @@
 package com.example.kok.hellofire;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.EditText;
 
 import java.util.Date;
 
-public class HelloFireActivity extends AppCompatActivity {
+public class HelloFireActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     public static final String EXTRA_MESSAGE = "com.example.kok.hellofire.MESSAGE";
+    private static int MY_REQUEST_CODE = 20181110;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    static Camera cam = null;
+    static Camera camera = null;
+    static SurfaceHolder cameraPreviewHolder = null;
     static boolean LIGHT_ON = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_fire);
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.CAMERA},
+                    MY_REQUEST_CODE);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("whj", "Now user should be able to use camera.");
+            }
+            else {
+                Log.d("whj", "Your app will not have this permission.");
+            }
+        }
+    }
+
 
     public void showCurrentTime(View view) {
         Intent intent = new Intent(this, ShowInfoActivity.class);
@@ -51,18 +78,19 @@ public class HelloFireActivity extends AppCompatActivity {
     }
 
     public void toggleLight(View view) {
-        int numberOfCameras = Camera.getNumberOfCameras();
-        Log.d("whj", String.format("Number of cameras: %d", numberOfCameras));
+        SurfaceView preview = findViewById(R.id.camera_light_view);
+        cameraPreviewHolder = preview.getHolder();
+        cameraPreviewHolder.addCallback(this);
+
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
-            Log.d("whj", "Camera flash hardware available.");
             if (!LIGHT_ON) {
                 try {
-                    cam = Camera.open(10);
-                    Camera.Parameters p = cam.getParameters();
+                    camera = Camera.open(0);
+                    Camera.Parameters p = camera.getParameters();
                     p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                    cam.setParameters(p);
-                    cam.startPreview();
-                    cam.autoFocus(new Camera.AutoFocusCallback() {
+                    camera.setParameters(p);
+                    camera.startPreview();
+                    camera.autoFocus(new Camera.AutoFocusCallback() {
                         public void onAutoFocus(boolean success, Camera camera) {
                         }
                     });
@@ -73,12 +101,36 @@ public class HelloFireActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             } else {
-                cam.stopPreview();
-                cam.release();
-                cam = null;
+                camera.stopPreview();
+                camera.release();
+                camera = null;
                 Log.d("whj", "Turn off camera light.");
                 LIGHT_ON = false;
             }
         }
     }
+
+    /**
+     * ========================================================
+     * Implement abstract function for SurfaceHolder.Callback
+     * ========================================================
+     */
+    public void surfaceCreated(SurfaceHolder holder) {
+        cameraPreviewHolder = holder;
+        try {
+            camera.setPreviewDisplay(cameraPreviewHolder);
+        } catch (Exception e) {
+            Log.d("whj", "Failed to set preview holder for camera.");
+            e.printStackTrace();
+        }
+    }
+
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // deliberately empty
+    }
+
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        cameraPreviewHolder = null;
+    }
+
 }
