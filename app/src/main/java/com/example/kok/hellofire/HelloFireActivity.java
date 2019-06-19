e1c5f18fe5f6237be2e90d2f10c178b4ecfb0b44
Hongjian <hongjian-wang@outlook.com>
2019-06-18 22:39:21 -0700

    Local notification example

--- app/src/main/java/com/example/kok/hellofire/HelloFireActivity.java
+++ app/src/main/java/com/example/kok/hellofire/HelloFireActivity.java
@@ -1,11 +1,17 @@
 package com.example.kok.hellofire;
 
 import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +26,7 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
 
     public static final String EXTRA_MESSAGE = "com.example.kok.hellofire.MESSAGE";
     private static int MY_REQUEST_CODE = 20181110;
+    private static String CHANNEL_ID = "WHJ";
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static Camera camera = null;
     static SurfaceHolder cameraPreviewHolder = null;
@@ -35,6 +42,7 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
             requestPermissions(new String[]{Manifest.permission.CAMERA},
                     MY_REQUEST_CODE);
         }
+        //createNotificationChannel();
     }
 
     @Override
@@ -51,6 +59,40 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
         }
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void setNotification(String msg) {
+        Intent intent = new Intent(this, HelloFireActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_name)
+                .setContentTitle("To HJ")
+                .setContentText(msg)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        int notificationID = (int) (new Date().getTime() / 1000); // unix timestamp in seconds
+        notificationManager.notify(notificationID, builder.build());
+    }
+
 
     public void showCurrentTime(View view) {
         Intent intent = new Intent(this, ShowInfoActivity.class);
@@ -58,6 +100,7 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
         String msg = c.toString();
         intent.putExtra(EXTRA_MESSAGE, msg);
         Log.d("whj", msg);
+        setNotification(msg);
         startActivity(intent);
     }
 
@@ -67,6 +110,7 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
         String msg = et.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, msg);
         Log.d("whj", msg);
+        setNotification(msg);
         startActivity(intent);
     }
 
@@ -78,9 +122,9 @@ public class HelloFireActivity extends AppCompatActivity implements SurfaceHolde
     }
 
     public void toggleLight(View view) {
-        SurfaceView preview = findViewById(R.id.camera_light_view);
-        cameraPreviewHolder = preview.getHolder();
-        cameraPreviewHolder.addCallback(this);
+//        SurfaceView preview = findViewById(R.id.camera_light_view);
+//        cameraPreviewHolder = preview.getHolder();
+//        cameraPreviewHolder.addCallback(this);
 
         if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
             if (!LIGHT_ON) {
